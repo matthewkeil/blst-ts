c22d33a9448964e0093abe7854d17452144d8b1d
matthewkeil <me@matthewkeil.com>
2022-11-06 19:56:55 -0600

    refactor(addon): remove boilerplate from yoman

--- src/addon/blst_ts_addon.cc
+++ src/addon/blst_ts_addon.cc
@@ -1,6 +1,6 @@
 #include "sodium.h"
-
 #include "../../blst/bindings/blst.h"
+
 #include "blst_ts_addon.h"
 
 int randomBytesNonZero(byte *out, int length)
@@ -16,60 +16,6 @@ int randomBytesNonZero(byte *out, int length)
     }
 };
 
-BlstTsAddon::BlstTsAddon(const Napi::CallbackInfo &info) : ObjectWrap(info)
-{
-    Napi::Env env = info.Env();
-
-    if (info.Length() < 1)
-    {
-        Napi::TypeError::New(env, "Wrong number of arguments")
-            .ThrowAsJavaScriptException();
-        return;
-    }
-
-    if (!info[0].IsString())
-    {
-        Napi::TypeError::New(env, "You need to name yourself")
-            .ThrowAsJavaScriptException();
-        return;
-    }
-
-    this->_greeterName = info[0].As<Napi::String>().Utf8Value();
-}
-
-Napi::Value BlstTsAddon::Greet(const Napi::CallbackInfo &info)
-{
-    Napi::Env env = info.Env();
-
-    if (info.Length() < 1)
-    {
-        Napi::TypeError::New(env, "Wrong number of arguments")
-            .ThrowAsJavaScriptException();
-        return env.Null();
-    }
-
-    if (!info[0].IsString())
-    {
-        Napi::TypeError::New(env, "You need to introduce yourself to greet")
-            .ThrowAsJavaScriptException();
-        return env.Null();
-    }
-
-    Napi::String name = info[0].As<Napi::String>();
-
-    printf("Hello %s\n", name.Utf8Value().c_str());
-    printf("I am %s\n", this->_greeterName.c_str());
-
-    return Napi::String::New(env, this->_greeterName);
-}
-
-Napi::Function BlstTsAddon::GetClass(Napi::Env env)
-{
-    return DefineClass(env, "BlstTsAddon", {
-                                               BlstTsAddon::InstanceMethod("greet", &BlstTsAddon::Greet),
-                                           });
-}
-
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
     if (sodium_init() < 0)
@@ -82,7 +28,6 @@ Napi::Object Init(Napi::Env env, Napi::Object exports)
     byte randomBytes[size];
     randomBytesNonZero(randomBytes, size);
 
-    exports.Set(Napi::String::New(env, "BlstTsAddon"), BlstTsAddon::GetClass(env));
     exports.Set(Napi::String::New(env, "randomBytes"), Napi::Buffer<char>::Copy(env, (char *)randomBytes, size));
 
     return exports;
--- src/addon/blst_ts_addon.h
+++ src/addon/blst_ts_addon.h
@@ -2,14 +2,4 @@
 
 #include <napi.h>
 
-class BlstTsAddon : public Napi::ObjectWrap<BlstTsAddon>
-{
-public:
-    BlstTsAddon(const Napi::CallbackInfo&);
-    Napi::Value Greet(const Napi::CallbackInfo&);
-
-    static Napi::Function GetClass(Napi::Env);
-
-private:
-    std::string _greeterName;
-};
+int randomBytesNonZero(byte *out, int length);
