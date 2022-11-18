d66afd7ceb77da161ae681f62149fa785dfbe81d
matthewkeil <me@matthewkeil.com>
2022-11-17 21:19:04 -0600

    feat: watch and rebuild blst

--- bin/dev.ts
+++ bin/dev.ts
@@ -2,39 +2,73 @@
 import {resolve} from "path";
 import {exec, watchFolder} from "../lib";
 
-const SRC_FOLDER = resolve(__dirname, "..", "src");
+const ROOT_FOLDER = resolve(__dirname, "..");
+const SRC_FOLDER = resolve(ROOT_FOLDER, "src");
 const ADDON_FOLDER = resolve(SRC_FOLDER, "addon");
+
+const BLST_FOLDER = resolve(ROOT_FOLDER, "blst");
+const BLST_SRC_FOLDER = resolve(BLST_FOLDER, "src");
+
 const DEBOUNCE_TIME = 100;
 
-let debounceCompile: undefined | NodeJS.Timeout;
-async function compile(): Promise<void> {
-  if (!debounceCompile) {
-    debounceCompile = setTimeout(() => {
-      if (debounceCompile) {
-        clearTimeout(debounceCompile);
+let debounceCompileAddon: undefined | NodeJS.Timeout;
+async function compileAddon(): Promise<void> {
+  if (!debounceCompileAddon) {
+    debounceCompileAddon = setTimeout(() => {
+      if (debounceCompileAddon) {
+        clearTimeout(debounceCompileAddon);
       }
-      debounceCompile = undefined;
+      debounceCompileAddon = undefined;
     }, DEBOUNCE_TIME);
     exec("npm run build:addon").then(console.log).catch(console.error);
   }
 }
 
+let debounceCompileBlst: undefined | NodeJS.Timeout;
+async function compileBlst(): Promise<void> {
+  if (!debounceCompileBlst) {
+    debounceCompileBlst = setTimeout(() => {
+      if (debounceCompileBlst) {
+        clearTimeout(debounceCompileBlst);
+      }
+      debounceCompileBlst = undefined;
+    }, DEBOUNCE_TIME);
+    await exec(`cd ${BLST_FOLDER} && sh build.sh`).then(console.log).catch(console.error);
+    await compileAddon();
+  }
+}
+
 async function buildWatchAddon(): Promise<void> {
-  // run node-gyp compile
-  void compile();
   // watch src folder and rerun compile on file changes
   return watchFolder({
     path: ADDON_FOLDER,
-    onAdd: () => compile(),
-    onChange: () => compile(),
-    onUnlink: () => compile(),
-    onUnlinkDir: () => compile(),
+    onAdd: () => compileAddon(),
+    onChange: () => compileAddon(),
+    onUnlink: () => compileAddon(),
+    onUnlinkDir: () => compileAddon(),
     onAddDir: () => {
       /* no-op */
     },
   });
 }
 
-if (require.main === module) {
-  void buildWatchAddon();
+async function buildWatchBlst(): Promise<void> {
+  // watch src folder and rerun compile on file changes
+  return watchFolder({
+    path: BLST_SRC_FOLDER,
+    onAdd: () => compileBlst(),
+    onChange: () => compileBlst(),
+    onUnlink: () => compileBlst(),
+    onUnlinkDir: () => compileBlst(),
+    onAddDir: () => {
+      /* no-op */
+    },
+  });
 }
+
+void (async function () {
+  await compileBlst();
+  await compileAddon();
+  void buildWatchBlst();
+  void buildWatchAddon();
+})();
