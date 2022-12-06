12d09a284fff4af6bf627d3415472066fe001680
matthewkeil <me@matthewkeil.com>
2022-12-05 20:38:12 -0600

    feat(addon): add exec, watch and dev scripts

---
+++ src/scripts/dev.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+import {resolve} from "path";
+import {watchWithCallback} from "./watch";
+import {cmdStringExec} from "./exec";
+
+const ROOT_FOLDER = resolve(__dirname, "..", "..");
+const SRC_FOLDER = resolve(ROOT_FOLDER, "src");
+const BINDINGS_FOLDER = resolve(SRC_FOLDER, "bindings");
+
+const BUILD_FOLDER = resolve(ROOT_FOLDER, "build", "Release");
+
+const DEBOUNCE_TIME = 200;
+
+/**
+ * Builds addon and then starts watch.
+ * Watches src/addon folder and rerun compile on file changes
+ */
+void watchWithCallback({
+  path: BINDINGS_FOLDER,
+  debounceTime: DEBOUNCE_TIME,
+  cb: () => cmdStringExec("npm run build:addon", false).then(console.log).catch(console.error),
+});
+
+void watchWithCallback({
+  path: BUILD_FOLDER,
+  debounceTime: DEBOUNCE_TIME,
+  cb: () =>
+    cmdStringExec("npm run test:bindings", false)
+      .then(console.log)
+      .catch(() => void 0),
+});
--- src/scripts/exec.ts
+++ src/scripts/exec.ts
@@ -1,8 +1,8 @@
-import child_process from "child_process";
+import {execFile, ExecOptions, exec as EXEC, ChildProcess, PromiseWithChild} from "child_process";
 
-export async function exec(cmd: string, args: string[], options?: child_process.ExecOptions): Promise<string> {
+export async function exec(cmd: string, args: string[], options?: ExecOptions): Promise<string> {
   return new Promise((resolve, reject): void => {
-    const proc = child_process.execFile(
+    const proc = execFile(
       cmd,
       args,
       {
@@ -19,4 +19,41 @@ export async function exec(cmd: string, args: string[], options?: child_process.
     if (proc.stdout) proc.stdout.pipe(process.stdout);
     if (proc.stderr) proc.stderr.pipe(process.stderr);
   });
+}
+
+export function cmdStringExec(command: string, logToConsole = true): PromiseWithChild<string> {
+  let child!: ChildProcess;
+  const promise = new Promise<string>((resolve, reject) => {
+    const chunks: Buffer[] = [];
+    function bufferOutput(data: string): void {
+      chunks.push(Buffer.from(data));
+    }
+    function stdoutHandler(data: string): void {
+      // eslint-disable-next-line no-console
+      console.log(data);
+    }
+    function stderrHandler(data: string): void {
+      // eslint-disable-next-line no-console
+      console.error(data);
+    }
+
+    child = EXEC(command, (err) => {
+      child.stdout?.removeListener("data", logToConsole ? stdoutHandler : bufferOutput);
+      child.stderr?.removeListener("data", logToConsole ? stderrHandler : bufferOutput);
+      const output = Buffer.concat(chunks).toString("utf8");
+      if (err) {
+        return logToConsole ? reject(err) : reject(output);
+      }
+      return logToConsole ? resolve("") : resolve(output);
+    });
+
+    if (child.stdin) {
+      process.stdin.pipe(child.stdin);
+    }
+    child.stdout?.on("data", logToConsole ? stdoutHandler : bufferOutput);
+    child.stderr?.on("data", logToConsole ? stderrHandler : bufferOutput);
+  }) as PromiseWithChild<string>;
+
+  promise.child = child;
+  return promise;
 }
