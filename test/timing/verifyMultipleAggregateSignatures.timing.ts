f15b4b68350d29199394481d30a803150e0eff13
matthewkeil <me@matthewkeil.com>
2022-12-15 23:43:22 -0500

    debugging workflow

--- test/timing/verifyMultipleAggregateSignatures.timing.ts
+++ test/timing/verifyMultipleAggregateSignatures.timing.ts
@@ -1,5 +1,5 @@
-import * as swigBindings from "../../src/swig/lib";
-import {functions} from "../../src/lib/bindings";
+// import * as swigBindings from "../../src/swig/lib";
+import {SecretKey, functions} from "../../src/lib/bindings";
 import {SignatureSet} from "../../src/lib/bindings.types";
 
 interface TestTime {
@@ -12,46 +12,46 @@ const msg = Buffer.from("i'm a little tea pot");
 function makeSets(numSets: number): SignatureSet[] {
   const sets: SignatureSet[] = [];
   for (let i = 0; i < numSets; i++) {
-    const sk = swigBindings.SecretKey.fromKeygen(Buffer.from("*".repeat(32)));
-    const publicKey = sk.toPublicKey();
+    const sk = SecretKey.keygen(Buffer.from("*".repeat(32)));
+    const publicKey = sk.getPublicKey();
     const signature = sk.sign(msg);
     sets.push({msg, publicKey: publicKey.serialize(), signature: signature.serialize()});
   }
   return sets;
 }
-function makeSwigSet(set: SignatureSet): swigBindings.SignatureSet {
-  return {
-    msg: set.msg,
-    pk: swigBindings.PublicKey.fromBytes(set.publicKey),
-    sig: swigBindings.Signature.fromBytes(set.signature),
-  };
-}
-async function swigSingleTest(sets: SignatureSet[]): Promise<TestTime> {
-  const startTime = Date.now();
-  const res = swigBindings.verifyMultipleAggregateSignatures(sets.map(makeSwigSet));
-  if (!res) {
-    throw new Error("invalid verification");
-  }
-  const endTime = Date.now();
-  return {
-    startTime,
-    endTime,
-  };
-}
-async function swigLoopTest(sets: SignatureSet[], loopCount: number): Promise<TestTime> {
-  const startTime = Date.now();
-  for (let i = 0; i < loopCount; i++) {
-    const res = swigBindings.verifyMultipleAggregateSignatures(sets.map(makeSwigSet));
-    if (!res) {
-      throw new Error("invalid verification");
-    }
-  }
-  const endTime = Date.now();
-  return {
-    startTime,
-    endTime,
-  };
-}
+// function makeSwigSet(set: SignatureSet): swigBindings.SignatureSet {
+//   return {
+//     msg: set.msg,
+//     pk: swigBindings.PublicKey.fromBytes(set.publicKey),
+//     sig: swigBindings.Signature.fromBytes(set.signature),
+//   };
+// }
+// async function swigSingleTest(sets: SignatureSet[]): Promise<TestTime> {
+//   const startTime = Date.now();
+//   const res = swigBindings.verifyMultipleAggregateSignatures(sets.map(makeSwigSet));
+//   if (!res) {
+//     throw new Error("invalid verification");
+//   }
+//   const endTime = Date.now();
+//   return {
+//     startTime,
+//     endTime,
+//   };
+// }
+// async function swigLoopTest(sets: SignatureSet[], loopCount: number): Promise<TestTime> {
+//   const startTime = Date.now();
+//   for (let i = 0; i < loopCount; i++) {
+//     const res = swigBindings.verifyMultipleAggregateSignatures(sets.map(makeSwigSet));
+//     if (!res) {
+//       throw new Error("invalid verification");
+//     }
+//   }
+//   const endTime = Date.now();
+//   return {
+//     startTime,
+//     endTime,
+//   };
+// }
 async function napiSingleTest(sets: SignatureSet[]): Promise<TestTime> {
   const startTime = Date.now();
   const res = await functions.verifyMultipleAggregateSignatures(sets);
@@ -87,15 +87,15 @@ async function napiLoopTest(sets: SignatureSet[], loopCount: number): Promise<Te
 
 void (async function () {
   const sets = makeSets(1000);
-  const swigSingle = await swigSingleTest(sets);
+  // const swigSingle = await swigSingleTest(sets);
   const napiSingle = await napiSingleTest(sets);
   const loopCount = 1;
-  const swigLoop = await swigLoopTest(sets, loopCount);
+  // const swigLoop = await swigLoopTest(sets, loopCount);
   const napiLoop = await napiLoopTest(sets, loopCount);
   console.log({
-    swig: swigSingle.endTime - swigSingle.startTime,
+    // swig: swigSingle.endTime - swigSingle.startTime,
     napi: napiSingle.endTime - napiSingle.startTime,
-    swigLoop: swigLoop.endTime - swigLoop.startTime,
+    // swigLoop: swigLoop.endTime - swigLoop.startTime,
     napiLoop: napiLoop.endTime - napiLoop.startTime,
   });
 })();
--- test/unit/newBindings/verifyMultipleAggregateSignatures.test.ts
+++ test/unit/newBindings/verifyMultipleAggregateSignatures.test.ts
@@ -1,32 +1,32 @@
-import {expect} from "chai";
-import {functions} from "../../../src/lib/bindings";
-import {getBindingTestSets} from "../../__fixtures__";
+// import {expect} from "chai";
+// import {functions} from "../../../src/lib/bindings";
+// import {getBindingTestSets} from "../../__fixtures__";
 
-describe("verifyMultipleAggregateSignaturesWorker()", () => {
-  it("should exist", () => {
-    expect(typeof functions.verifyMultipleAggregateSignatures).to.equal("function");
-  });
-  it("should return a Promise<boolean>", async () => {
-    const resPromise = functions.verifyMultipleAggregateSignatures([]);
-    expect(resPromise).to.be.instanceOf(Promise);
-    const result = await resPromise;
-    expect(typeof result).to.equal("boolean");
-  });
-  it("should default to false", async () => {
-    const result = await functions.verifyMultipleAggregateSignatures([]);
-    expect(result).to.be.false;
-  });
-  it("should return true for valid sets", async () => {
-    const sets = getBindingTestSets(10).map((set) => {
-      const {msg, swig} = set;
-      const {publicKey, signature} = swig;
-      return {
-        msg,
-        publicKey: publicKey.serialize(),
-        signature: signature.serialize(),
-      };
-    });
-    const res = await functions.verifyMultipleAggregateSignatures(sets);
-    expect(res).to.be.true;
-  });
-});
+// describe("verifyMultipleAggregateSignaturesWorker()", () => {
+//   it("should exist", () => {
+//     expect(typeof functions.verifyMultipleAggregateSignatures).to.equal("function");
+//   });
+//   it("should return a Promise<boolean>", async () => {
+//     const resPromise = functions.verifyMultipleAggregateSignatures([]);
+//     expect(resPromise).to.be.instanceOf(Promise);
+//     const result = await resPromise;
+//     expect(typeof result).to.equal("boolean");
+//   });
+//   it("should default to false", async () => {
+//     const result = await functions.verifyMultipleAggregateSignatures([]);
+//     expect(result).to.be.false;
+//   });
+//   it("should return true for valid sets", async () => {
+//     const sets = getBindingTestSets(10).map((set) => {
+//       const {msg, swig} = set;
+//       const {publicKey, signature} = swig;
+//       return {
+//         msg,
+//         publicKey: publicKey.serialize(),
+//         signature: signature.serialize(),
+//       };
+//     });
+//     const res = await functions.verifyMultipleAggregateSignatures(sets);
+//     expect(res).to.be.true;
+//   });
+// });
